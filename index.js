6f5e156d90ed37a1e5bd0f856199a6c736896d22
dilane3 <komboudilane125@gmail.com>
2021-07-15 23:38:07 +0100

    amelioration

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const http = require("http");
 const socketio = require("socket.io");
 const router = require("./router");
 
-const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
